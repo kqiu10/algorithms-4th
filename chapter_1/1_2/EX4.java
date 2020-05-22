b06f0db6810f9ad5a6dd8c745d3335479a52425d
kqiu10 <qiukan1011@126.com>
2020-05-22 13:14:24 -0400

    1.2.4_1.2.5

---
+++ chapter_1/1_2/EX4.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class EX4{
+    public static void main(String[] args){
+    String string1 ="hello";
+    String string2 = string1;
+    string1 ="world";
+    StdOut.println(string1);
+    StdOut.println(string2);
+    }
+
+}
---
+++ chapter_1/1_2/EX5.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class EX5{
+    public static void main(String[] args){
+    String s ="Hello World";
+    s.toUpperCase();
+    s.substring(6,11);
+    StdOut.println(s);
+    }
+}
