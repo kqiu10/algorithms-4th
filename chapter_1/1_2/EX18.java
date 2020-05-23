88aa1b920c03e54c8a5bec1aacf3b6fa6fa9b961
kqiu10 <qiukan97@gmail.com>
2020-05-23 12:20:52 -0400

    1.2.18

---
+++ chapter_1/1_2/EX18.java
@@ -0,0 +1,27 @@
+import edu.princeton.cs.algs4.StdIn;
+public class EX18{
+  private double m;
+  private double s;
+  private int N;
+  public void addDataValue(double x){
+  N++;
+  s = s + 1.0 * (N-1) / N* (x - m)*(x-m);
+  m = m +(x -m) /N;
+  }
+  public double mean(){return m;};
+  public double var(){return s/(N-1);}
+  public double stddev(){return Math.sqrt(this.var());}
+  public static void main(String[] args){
+      EX18 accumulator = new EX18();
+      while(!StdIn.isEmpty()){
+      accumulator.addDataValue(StdIn.readDouble());
+      }
+        System.out.println("Mean: " + accumulator.mean());
+  System.out.println("Var: " + accumulator.var());
+  System.out.println("Stddev: " + accumulator.stddev());
+  
+  
+  }
+  
+
+}
