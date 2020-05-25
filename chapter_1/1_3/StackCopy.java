f357a2207f40439ec319c7e295ca0d78d76a8899
kqiu10 <qiukan97@gmail.com>
2020-05-25 12:22:21 -0400

    1.3.12

---
+++ chapter_1/1_3/StackCopy.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+public class StackCopy{  
+    public static Stack copy(Stack<String> origin){
+        Stack<String> copy = new Stack<>();
+        for(String s :  origin)
+            copy.push(s);
+        return copy;   
+    }
+    public static void main(String[] args){
+    Stack<String> stack = new Stack<>();
+    while(!StdIn.isEmpty()){
+    String s = StdIn.readString();
+    stack.push(s);
+    Stack<String> newstack =copy(stack);
+    StdOut.println("the origin stack is");
+    StdOut.println(stack);
+    StdOut.println("the copy stack is");
+    StdOut.println(newstack);   
+    }   
+    }
+ 
+}
