460efd8a1d4e4c3849ece4d7a53e3c8a3e2bade9
kqiu10 <qiukan97@gmail.com>
2020-05-29 13:31:28 -0400

    1.3.30

--- chapter_1/1_3/reverse.java
+++ chapter_1/1_3/reverse.java
@@ -1,6 +1,6 @@
 public class reverse{
     public Node reverse(Node node){
-    Node first = x;
+    Node first = node;
     Node reverse = null;
     while(first != null)
     {
@@ -14,6 +14,18 @@ public class reverse{
     
     
     }
+    public Node reverseRecursion(Node first){
+     if(first == null) return null;
+        if(first.next == null)return first;
+        Node second = first.next;
+        Node rest = reverseRecursion(second);
+        second.next = first;
+        first.next = null;
+        return rest;
 
-
+    
+    }
+    
+    
+   
 }
