db3723ebac88bc861f3547c0f20bf08b8cb6282b
kqiu10 <qiukan97@gmail.com>
2020-06-24 23:53:49 -0400

    finish part of 2.1

---
+++ chapter_2/2_1/EX1.java
@@ -0,0 +1,9 @@
+
+
+public class EX1 {
+	public static void main(String[] args) {
+		System.out.println((0 + 15) / 2);
+		System.out.println(2.0e-6 * 100000000.1);
+		System.out.println(true && false || true && true);
+	}
+}
