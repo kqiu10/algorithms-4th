56ec2cc265f037c13cb1f33674f2c02036836724
kqiu10 <qiukan97@gmail.com>
2020-06-30 13:48:33 -0400

    finish Permutation

--- chapter_2/2_1/Permutation.java
+++ chapter_2/2_1/Permutation.java
@@ -4,8 +4,27 @@
  *  Description: takes an integer k as a command-line argument; reads a sequence of strings from standard input using StdIn.readString(); and prints exactly k of them, uniformly at random. Print each item from the sequence at most once.
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        final int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> res = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (k == 0) {
+                return;
+            }
+            else {
+                res.enqueue(s);
+            }
+
+        }
+        while (!res.isEmpty()) {
+            StdOut.println(res.dequeue());
+        }
+
 
     }
 }
