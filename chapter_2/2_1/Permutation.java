cbef559de801fa022452d4e3458b60e5258cf8fa
kqiu10 <qiukan97@gmail.com>
2020-06-29 22:21:55 -0400

    finish part of Permutation

---
+++ chapter_2/2_1/Permutation.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:kane
+ *  Date:6/26
+ *  Description: takes an integer k as a command-line argument; reads a sequence of strings from standard input using StdIn.readString(); and prints exactly k of them, uniformly at random. Print each item from the sequence at most once.
+ **************************************************************************** */
+
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+}
